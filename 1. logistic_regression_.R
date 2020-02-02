1170a2d4fb140006d22b776e5a982957c24433b5
areumsim <31869418+areumsim@users.noreply.github.com>
2020-02-03 00:43:25 +0900

    Update and rename logistic_regression_.R to 1. logistic_regression_.R

--- 1. logistic_regression_.R
+++ 1. logistic_regression_.R
@@ -1,3 +1,4 @@
+### Test Data
 set.seed(2016)
 x0 = rep(1,30) #bias
 x1 = rnorm(30,3,2) + 0.1*c(1:30)
@@ -6,19 +7,21 @@ x3 = rpois(n = 30, lambda = 4)
 x3[16:30] = x3[16:30] - rpois(n = 15, lambda = 2)
 X = cbind(x0,x1,x2,x3)
 
+  # dependent variable 
 y = c(rbinom(5, 1,0.1),rbinom(10, 1,0.25),rbinom(10, 1,0.75),rbinom(5, 1,0.9))
 
 
-logistic_regression_ <- function(X, y)
-{
-  calc_p <- function(X,beta)
+logistic_regression_ <- function(X, y) {
+  calc_p <- function(X, beta)
   {
     beta <- as.vector(beta)
     return ( exp(X%*%beta) / (1+ exp(X%*%beta)) )
   }  
   
+  # initial beta
   beta <- rep(0, ncol(X))
   iter_count <- 0
+  
   repeat{
     #calculate probabilities using current estimate of beta
     p <- as.vector(calc_p(X, beta))
@@ -44,8 +47,7 @@ logistic_regression_ <- function(X, y)
 
 logistic_regression_(X,y)
 
-
-#using R package 
-M1 = glm(y~x1+x2+x3, family = "binomial")
-M1$coefficients
+#Refer. using R package 
+model = glm(y~x1+x2+x3, family = "binomial")
+model$coefficients
 
